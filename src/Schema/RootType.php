ac33a33db08389af5e8f2ccf13c263e2235e1466
andershagbard <andershagbard@gmail.com>
2022-02-24 15:21:45 +0100

    Add getName method

--- src/Schema/RootType.php
+++ src/Schema/RootType.php
@@ -2,11 +2,27 @@
 
 namespace Nuwave\Lighthouse\Schema;
 
+use Exception;
+
 class RootType
 {
     public const QUERY = 'Query';
     public const MUTATION = 'Mutation';
     public const SUBSCRIPTION = 'Subscription';
+
+    public static function getName(string $typeName): string
+    {
+        switch ($typeName) {
+            case static::QUERY:
+                return config('lighthouse.namespaces.queries');
+            case static::MUTATION:
+                return config('lighthouse.namespaces.mutations');
+            case static::SUBSCRIPTION:
+                return config('lighthouse.namespaces.subscriptions');
+            default:
+                throw new Exception('RootType of ' . $typeName . ' was not found');
+        }
+    }
 
     public static function isRootType(string $typeName): bool
     {
